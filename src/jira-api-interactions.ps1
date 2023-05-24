497042380bdc83f41dbbf5185e8972a94fd10614
Brady Clifford <8923247+bradyclifford@users.noreply.github.com>
2023-05-24 09:50:38 -0600

    Update jira-api-interactions.ps1

--- src/jira-api-interactions.ps1
+++ src/jira-api-interactions.ps1
@@ -1,5 +1,4 @@
 function Invoke-JiraQuery {
-    [OutputType([boolean])]
     Param (
         [Uri]$Query,
         [string]$Username = "",
@@ -7,18 +6,51 @@ function Invoke-JiraQuery {
     );
 
     Write-Host "Querying the Jira API $($Query.AbsoluteUri)"
-    
-    $arguments = @{
-      Uri = $Query
-      SkipHttpErrorCheck = $true
-      StatusCodeVariable = "statusCode"
-    }
-    
+
+    $queryResult = $null
+
     if ($Username -ne "") {
         $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $Password
         $plainPassword = $cred.GetNetworkCredential().Password
 
         # Prepare the Basic Authorization header - PSCredential doesn't seem to work
+        $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username, $plainPassword)))
+        $headers = @{Authorization = ("Basic {0}" -f $base64AuthInfo) }
+    
+        # Execute the query with the auth header
+        Invoke-RestMethod -Uri $Query -Headers $headers
+    }
+    else {
+        # Execute the query
+        Invoke-RestMethod -Uri $Query
+    }
+
+    return $queryResult
+}
+
+function Invoke-JiraTicketTransition {
+    Param (
+        [Uri]$Uri,
+        [string]$Body,
+        [string]$Username = "",
+        [securestring]$Password
+    );
+
+    Write-Host "Posting to the Jira API $($Uri.AbsoluteUri)"
+    
+    $arguments = @{
+      Uri = $Query
+      Body = $Body
+      Method = "Post"
+      ContentType = "application/json"
+      UseBasicParsing = $true
+      SkipHttpErrorCheck = $true
+      StatusCodeVariable = "statusCode"
+    }
+
+    if ($Username -ne "") {
+        $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $Password
+        $plainPassword = $cred.GetNetworkCredential().Password
         $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username, $plainPassword)))
         $headers = @{Authorization = ("Basic {0}" -f $base64AuthInfo) }
         $arguments.Add("Headers", $headers)
@@ -35,30 +67,5 @@ function Invoke-JiraQuery {
       return false
     }
     
-    throw "Jira request failed with status code [$statusCode]"
-}
-
-function Invoke-JiraTicketTransition {
-    Param (
-        [Uri]$Uri,
-        [string]$Body,
-        [string]$Username = "",
-        [securestring]$Password
-    );
-
-    Write-Host "Posting to the Jira API $($Uri.AbsoluteUri)"
-
-    if ($Username -ne "") {
-        $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $Password
-        $plainPassword = $cred.GetNetworkCredential().Password
-        $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username, $plainPassword)))
-        $headers = @{Authorization = ("Basic {0}" -f $base64AuthInfo) }
-
-        # Send a POST with the auth header
-        Invoke-RestMethod -Uri $Uri -Headers $headers -UseBasicParsing -Body $Body -Method Post -ContentType "application/json"
-    }
-    else {
-        # Send a POST
-        Invoke-RestMethod -Uri $Uri -UseBasicParsing -Body $Body -Method Post -ContentType "application/json"
-    }
+    throw "Jira ticket transition failed with status code [$statusCode]"
 }
